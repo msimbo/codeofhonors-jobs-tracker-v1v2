12d0823e2b312704b4268c6a8c52c3f9de7c9914
Abeco Jean Claude <73258089+abecojc@users.noreply.github.com>
2021-01-14 11:17:57 -0500

    added search page

---
+++ app.js
@@ -0,0 +1,62 @@
+
+let keywordIn = document.querySelector('#keyword')
+let locations = document.querySelector('#location')
+let search = document.querySelector('#submit')
+let searchMenu = document.querySelector('#searchPage')
+// let input = document.querySelector('#input')
+let jobListing = document.querySelector('#display')
+
+console.log('1',keywordIn.value)
+console.log('11',locations.value)
+let jobList = []
+
+const errorMsg = document.createElement("div")
+errorMsg.innerText=`Invalid`
+console.log('2',errorMsg.value)
+jobListing.appendChild(errorMsg)
+console.log('22',jobListing.value)
+
+// const MAX_CAPACITY = 2
+
+search.addEventListener('submit', function (e) {
+    e.preventDefault()
+    keywordIn.value = ""
+    locations.value = ""
+
+    if (keywordIn.value===''||locations.value===''){
+        // length < MAX_CAPACITY) {
+        errorMsg.className = 'bg-red-500 text-white'
+        search.className = 'disabled bg-gray-500 rounded cursor-not-allowed'
+        console.log('3',keywordIn.value)
+        return errorMsg
+    } else {
+        return jobList[10]
+    }
+})
+
+// function displayError(){
+//
+// }
+
+async function getJobs() {
+    const url = 'https://api.adzuna.com/v1/api/jobs/us/search/1?app_id={d9bee79e}&app_key={a8eea83bb5dbb169da71decb4c8cac85}'
+    const searchResult = await fetch(url)
+    const dataReturned = await searchResult.json()
+    console.log(dataReturned)
+
+    // const i = [10]
+    const jobDetails = {
+
+        job:`${dataReturned[0]}`
+        // Job:`${dataReturned.results[0].title}
+        //      ${dataReturned.results[0].description}
+        //       ${dataReturned.results[0].location}`
+
+    }
+    jobList.push(jobDetails)
+}
+jobList.forEach(showResult)
+function showResult(){
+    searchMenu.nextElementSibling.classList.remove("hidden")
+    jobListing.innerHTML=`${jobList[0]}`
+}
