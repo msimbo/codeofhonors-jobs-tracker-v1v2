46ef11e6065b53b5f50f6ca151dbe093d8e07359
edeced <ed_ced@icloud.com>
2021-01-14 05:26:25 -0600

    adds login-register  files, html & js

---
+++ login-app.js
@@ -0,0 +1,79 @@
+// 1. get DOM element
+const form = document.querySelector('#form')
+
+form.addEventListener('submit', function (e) {
+  e.preventDefault()
+
+  validateEmpty(username)
+  validateEmpty(email)
+  validateEmpty(password1)
+  // validateEmpty(password2)
+  // validateMinLength(username)
+  // validateMinLength(password2)
+  // validateIsEmail(email)
+  validatePassMatch(confirm_password, password)
+
+  //@TODO: Code Challenge 5c: Starting from your refactored code,
+  // 1. Implement and use the  validatePassMatch() function to confirm both passwords match
+  // 2. Implement and use the emailValidates() function to confirm it's a valid email. See: https://stackoverflow.com/a/46181
+
+  //@TODO: Code Challenge 5b: Refactor your CC 5a to use function with the "blueprints" below
+  // validateIsEmail(email)
+
+})
+
+function validateEmpty(input) { 
+  if (input.value === '') {
+    showError(input, `${input.name} is empty`)
+  } else {
+    showSuccess(input)
+  }
+}
+
+function showError (input, msg) {
+  input.nextElementSibling.innerHTML = `<small class="text-white uppercase">❌ ${msg}</small>`
+  input.className='error'
+}
+
+function showSuccess (input) {
+  input.nextElementSibling.innerHTML = `<small class="text-white ">✅ </small>`
+  input.className= 'success-input'
+}
+
+function validateIsEmail (email) {
+  // const re = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+  // console.log(re)
+  if (emailValidates(email)) {
+    showSuccess(email)
+  } else {
+    showError(email)
+  }
+}
+
+function emailValidates (email) {
+  const re = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+  
+  return true
+}
+// console.log(emailValidates)
+  
+function validatePassMatch (password1,password2) {
+  console.log(passw)
+  if (password2.value != password1.value) {
+    showError(password2, "does not match")
+    console.log()
+  } else {
+  showSuccess(confirm_password,'Does not match')
+  }
+}
+
+
+function validateMinLength (input) {
+  console.log(input.value.length)
+  
+  if (input.value.length < 5 || input.value.length > 14) {
+  showError(input)
+  } else {
+    showSuccess(input)
+  }
+}
