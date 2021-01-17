7e84ebab3a3a827233d24e390f7dfc8f86db9afc
Abeco Jean Claude <73258089+abecojc@users.noreply.github.com>
2021-01-17 12:04:21 -0500

    search page styling

--- search.js
+++ search.js
@@ -2,46 +2,94 @@ const searchForm = document.querySelector('#search-form')
 const jobType = document.querySelector('#job-type')
 const resultSection = document.querySelector('#result-section')
 const searchHeader = document.querySelector('#app')
-const searchFull = document.querySelector('#searchPage')
 const SEARCH_COUNTRY = 'us'
+//adds
+// const searchCity = document.querySelector('#location1')
+const searchFull = document.querySelector('#form')
+const keyword = document.querySelector('#keyword')
+const searchFormFull = document.querySelector('#search-page')
+searchFull.addEventListener('submit', async function (e) {
+  e.preventDefault()
 
+  const keyName = keyword.value
+
+  console.log(keyName)
+
+  let keyResults = await searchJobs(keyName)
+  console.log('1',keyResults)
+  let keywords = keyResults.results // this is an Array
+  console.log('2',keywords)
+  let keywordsFound = keyResults.count
+  console.log('3',keywordsFound)
+  resultSection.innerHTML = `
+
+  <div id="result-section" class="p-10">
+  
+      <h1>${keywordsFound} jobs found for <strong>${keyName}</strong> in ${SEARCH_COUNTRY.toUpperCase()}</h1>
+  
+  </div>`
+
+  keywords.forEach(function (key) {
+    const div = document.createElement('div')
+    div.innerHTML = `
+       <div class="rounded cursor-pointer border-2 border-blue-900 shadow-lg my-2 hover:bg-blue-400">
+          <h4 class="p-2 border-2 border-blue-400 bg-blue-500 text-white whitespace-nowrap shadow-2xl">
+                <a href="${key.redirect_url}">
+                       ${key.title}
+                </a> | ${key.location.display_name} 
+          </h4>
+           <p  class="p-2">
+               ${key.description}
+           </p>
+       </div>`
+    resultSection.appendChild(div)
+
+    searchHeader.classList.remove('hidden')
+    searchFormFull.classList.add('hidden')
+    // console.log(searchFull.classList)
+  })
+})
+//end adds
 
 searchForm.addEventListener('submit', async function (e) {
   e.preventDefault()
 
-  searchHeader.classList.remove('hidden')
-  searchFull.classList.add('hidden')
-console.log(searchFull.classList)
-
   const jobName = jobType.value
 
+  console.log(jobName)
+
   let jobResults = await searchJobs(jobName)
-
+  console.log('1',jobResults)
   let jobs = jobResults.results // this is an Array
-
+  console.log('2',jobs)
   let jobsFound = jobResults.count
-
+  console.log('3',jobsFound)
   resultSection.innerHTML = `
 
-<div id="result-section" class="p-10">
+  <div id="result-section" class="p-10">
+  
+      <h1>${jobsFound} jobs found for <strong>${jobName}</strong> in ${SEARCH_COUNTRY.toUpperCase()}</h1>
+  
+  </div>`
 
-<h1>${jobsFound} jobs found for <strong>${jobName}</strong> in ${SEARCH_COUNTRY.toUpperCase()}</h1>
-
-</div>`
   jobs.forEach(function (job) {
     const div = document.createElement('div')
     div.innerHTML = `
-       <div class="rounded cursor-pointer hover:bg-blue-400">
-          <h4 class="p-2 my-4 border-2 border-blue-400 whitespace-nowrap shadow-2xl">
+       <div class="rounded cursor-pointer border-2 border-blue-900 shadow-lg my-2 hover:bg-blue-400">
+          <h4 class="p-2 border-2 border-blue-400 bg-blue-500 text-white whitespace-nowrap shadow-2xl">
                 <a href="${job.redirect_url}">
                        ${job.title}
-                </a> | ${job.location.display_name}
+                </a> | ${job.location.display_name} 
           </h4>
-           <p>
+           <p  class="p-2">
                ${job.description}
            </p>
        </div>`
     resultSection.appendChild(div)
+
+    // searchHeader.classList.remove('hidden')
+    // // searchFull.classList.add('hidden')
+    // console.log(searchFull.classList)
   })
 })
 
